3b498d79557ae7fda709f1086b20cebc39fe9dae
tjtdev <tolguyture@gmail.com>
2024-07-18 12:25:31 -0600

    chapter2

---
+++ chapter2/src/demo.ts
@@ -0,0 +1,10 @@
+let x: number 
+let y: string
+let z: boolean
+let a: Date
+// let b: string[]
+let b: any //to assing any 
+
+
+b = "Hello!" //or you can say "as any" to get rid of error if you give specific var
+b = 1234 //shows when it is wrong var
