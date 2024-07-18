d0a76a86e81ddd51e48aadee81e4ddc9435e4003
tjtdev <tolguyture@gmail.com>
2024-07-18 12:44:32 -0600

    ch2section4

--- chapter2/ch2sec4/src/demo.ts
+++ chapter2/ch2sec4/src/demo.ts
@@ -2,22 +2,13 @@ interface Contact {
     id: number;
     name: ContactName;
     birthDate: Date; //add ? to make it optional, if you didnt want to include all the fields
-    status: ContactStatus;
 }
 
-enum ContactStatus {
-    Active = "active",
-    Inactive = "inactive",
-    New = "new"
-}
-
-//special type that has hard-coded list of values
 
 let primaryContact: Contact = {
     birthDate: new Date("01-01-1980"),
     id: 12345,
-    name: "TJ",
-    status: ContactStatus.Active
+    name: "TJ"
 }
 
 type ContactName = string
